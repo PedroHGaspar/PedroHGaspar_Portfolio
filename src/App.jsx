58879caf993be182deb028ee24d8ab92a3a12267
PedroHGaspar <ordep12@hotmail.com.br>
2023-03-17 10:18:07 -0300

    Fix style header. App.css no longer exists.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
-
-import './App.css'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
@@ -18,13 +15,13 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
   return (
     <nav
       className={`
-      ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}//needs to be inside `` so that can be a dinamic template string ||| top-0 make stay on the top, z-20 to make it appear above other elements.
+      ${styles.paddingX} w-full flex items-center justify-center py-5 fixed top-0 z-20 bg-primary`}//needs to be inside `` so that can be a dinamic template string ||| top-0 make stay on the top, z-20 to make it appear above other elements.
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">{/* max-w-7xl meaning a lot of width in the navbar. mx-auto is margin-x auto */}
+      <div className="w-full flex justify-between items-center max-w-7xl">{/* max-w-7xl meaning a lot of width in the navbar. mx-auto is margin-x auto */}
         <Link
           to="/"//meaning top of the page
           className="flex items-center gap-2"//gap-2 meaning utilities for controlling gutters between grid and flexbox items.(A propriedade css gap define os espaços (gutters (en-US)) entre as linhas e colunas)
@@ -30,28 +30,26 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />{/*w-9 e h-9 set the width and heigth */}
           <p className="text-white text-[18px] font-bold cursor-pointer flex">Pedro &nbsp;<span className="sm:block hidden"> |&nbsp; Front-End Developer</span></p>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((link) => (
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
             <li
-              key={link.id}
-              className={`${active === link.title
-                ? "text-white"
-                : "text-secondary"
-                } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(link.title)}
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
             >
-              <a href={`#${link.id}`}>{link.title}
-              </a>
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
             src={toggle ? close : menu}
-            alr="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
-            onClick={() => setToggle(!toggle)}//once we clicked, we want to change the state, so the setToggle turns into toggle
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
           />
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-10 my-0 min-w-[140px] z-10 rounded-xl`}>
             <ul className="list-none flex justify-end items-start flex-col gap-4">
