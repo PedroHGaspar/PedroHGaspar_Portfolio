ab8775bdc40152cadaa2c8871b9c097cf428a694
Pedro Gaspar <ordep12@hotmail.com.br>
2023-03-30 14:49:32 -0300

    BallCanvas is on FIRE!! The technology balls have been add

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+const Ball = (props) => {
+
+  const [decal] = useTexture([props.imgUrl])
+
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} /> {/* this icosahedronGeometry make a geometric form that has many sides  */}
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+        />
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}//this value needs to be there to properly render the modal.
+    >
+      <Suspense fallback={<CanvasLoader />}>{/*This is from react. This allow us to have a loader while our modal is loading.*/}
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default BallCanvas
