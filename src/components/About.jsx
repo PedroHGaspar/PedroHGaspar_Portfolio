39cc108ad912a177cd77a0366c067b7199ec4178
Pedro Henrique Gaspar <pedro.gaspar@penseavanti.com.br>
2023-03-30 09:12:24 -0300

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,6 +25,17 @@ const ServiceCard = ({ index, title, icon }) => {//in the p tag it doesn't need 
 const About = () => {
   return (
     <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >{/*here it works like this: direction, type, delay and duration of the animation */}
+        I'm a Front-End Jr. Developer with experience in HTML5, CSS3, TypeScript, JavaScript and expertise in frameworks like React, React Native, Node.js, Three.js and Tailwind CSS.
+        I'm a quick learner and i like to learn new things and improove my knowledge on the things i'm already using on the daily jobs.
+      </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
